4dc6d3f7984c6b69f85a729aeea1360c558c6bb5
dinasstanulis <dinas.stanulis@stud.techin.lt>
2025-01-08 06:15:43 +0200

    Update

--- 2.2/02_09.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ 2.2/02_09.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -5,8 +5,20 @@ public class SimpleCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number: ");
+        int numberFirst = Integer.parseInt(scanner.nextLine());
 
-        // Write your program here
+        System.out.println("Give the second number: ");
+        int numberSecond = Integer.parseInt(scanner.nextLine());
 
+        int sum = numberFirst + numberSecond;
+        int subtract = numberFirst - numberSecond;
+        int multiply = numberFirst * numberSecond;
+        double divide = (double) numberFirst / numberSecond;
+
+        System.out.println(numberFirst + " + " + numberSecond + "=" + sum);
+        System.out.println(numberFirst + " - " + numberSecond + "=" + subtract);
+        System.out.println(numberFirst + " * " + numberSecond + "=" + multiply);
+        System.out.println(numberFirst + " / " + numberSecond + "=" + divide);
     }
 }
