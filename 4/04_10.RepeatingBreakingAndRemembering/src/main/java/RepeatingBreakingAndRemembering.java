1de3f198f8224000321466f990f9af6c40240a8c
Dinas <dinas.stanulis@stud.techin.lt>
2025-01-08 23:39:02 +0200

    tweaks

--- 4/04_10.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ 4/04_10.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -1,18 +1,41 @@
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class RepeatingBreakingAndRemembering {
 
     public static void main(String[] args) {
-        
-        // This exercise is worth five exercise points, and it is 
-        // gradually extended part by part.
-        
-        // If you want, you can send this exercise to the server
-        // when it's just partially done. In that case the server will complain about 
-        // the parts you haven't done, but you'll get points for the finished parts.
-        
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int i = 0;
+        int evenAmount = 0;
+        int oddAmount = 0;
+        while (true) {
+            System.out.println("Give numbers:");
+            int number = scanner.nextInt();
+
+            if (number == -1) {
+                System.out.println("Thx! Bye!");
+                System.out.println("Sum: " + sum);
+                System.out.println("Numbers: " + i);
+                System.out.println("Even: " + evenAmount);
+                System.out.println("Odd: " + oddAmount);
+                if (i > 0) {
+                    double avg = (double) sum / i;
+                    System.out.println("Average: " + avg);
+                }
+                break;
+
+            }
+            sum += number;
+            i++;
+            if (number % 2 == 0) {
+                evenAmount++;
+            } else {
+                oddAmount++;
+            }
+            ;
+        }
     }
 }
