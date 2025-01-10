dfada43061578fffae59b994e934e9b7efce6700
Dinas Stanulis <dinas.stanulis@stud.techin.lt>
2025-01-10 08:58:17 +0200

    finish 5

--- 5/05_12.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ 5/05_12.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -22,13 +22,20 @@ public class AdvancedAstrology {
     }
 
     public static void christmasTree(int height) {
-
+        for (int i = 1; i <= height*2; i+=2) {
+            printSpaces(height-i/2-1);
+            printStars(i);
+        }
+        for (int i = 0; i <= 1; i++ ) {
+            printSpaces(((height*2-1)-3)/2);
+            printStars(3);
+        }
     }
 
     public static void main(String[] args) {
         // The tests are not checking the main, so you can modify it freely.
 
-        printTriangle(3);
+        printTriangle(4);
         System.out.println("---");
         christmasTree(4);
         System.out.println("---");
