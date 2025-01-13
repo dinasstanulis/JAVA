19eade6f9bec1866ca6b00f4d145780ce463ad36
Dinas Stanulis <dinas.stanulis@stud.techin.lt>
2025-01-13 14:20:49 +0200

    one task

---
+++ 6/06_3_arrays/src/main/java/lt/techin/praktinis/ArraysTask.java
@@ -0,0 +1,72 @@
+package lt.techin.praktinis;
+
+import java.util.Arrays;
+
+public class ArraysTask {
+
+
+    public static void main(String[] args) {
+        // Įgyvendikite visus metodus (turi pažaliuoti unit testai)
+        // Pateiktas masyvas ir pavyzdys programos patikrinimui. Masyvo elementų reikšmes galite keisti.
+
+        int[] arr = {3, 2, 1, 4, 6, 5};
+
+        int[] arrWithRemovedElement = removeElementAndReturnNewArray(arr, 1);
+        System.out.println(Arrays.toString(arrWithRemovedElement));
+
+        int size = 6;
+        int min = 1;
+        int max = 50;
+        System.out.println(Arrays.toString(generateRandomArray(6, 1, 50)));
+    }
+
+   // Parašykite metodą, kuris sukuria nustatyto dydžio sveikųjų skaičių masyvą
+   // ir užpildo jį atsitiktinai sugeneruotais skaičiais iš nurodyto intervalo.
+    public static int[] generateRandomArray(int size, int min, int max) {
+        int [] randomArray = new int [size];
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = (int)(Math.random()*(max-min)+min);;
+        }
+        return randomArray;
+    }
+
+
+    //Parašykite metodą, kuris pašalintų masyvo arr[n] k-tąjį elementą (su indeksu k),
+    //surašant reikiamus elementus į naują masyvą b[n-1]. Metodas grąžina naują masyvą.
+    //Jei k yra už masyvo ribų, metodas grąžina null (teisingiau būtų mesti exception, bet dar nesimokėm)
+    public static int[] removeElementAndReturnNewArray(int[] arr, int k) {
+        //TODO
+        return null;
+    }
+
+    //Parašykite metodą, kuris įterptų į masyvą arr[n] prieš k-tąjį elementą reikšmę x,
+    //surašant reikiamus elementus į naują masyvą b[n+1]. Metodas grąžina naują masyvą.
+    //Jei k yra už masyvo ribų, metodas grąžina null (teisingiau būtų mesti exception, bet dar nesimokėm)
+    public static int[] insertElementAndReturnNewArray(int[] arr, int k, int x){
+        //TODO
+        return null;
+    }
+
+    //Parašykite metodą, kuris grąžintų duoto masyvo apverstą kopija.
+    public static int[] reverseArray(int[] arr) {
+        //TODO
+        return null;
+    }
+
+    //Parašykite metodą, kuris apverstu duotą masyvą (nekuriant naujo masyvo)
+    public static void reverseInPlace(int[] arr) {
+        //TODO
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
