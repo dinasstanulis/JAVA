e5f8eab05ac56d1c22765119714ee190e8f675c8
Dinas <dinas.stanulis@stud.techin.lt>
2025-01-20 16:32:35 +0200

    x

--- 8/08_10.Multiplier/src/main/java/Multiplier.java
+++ 8/08_10.Multiplier/src/main/java/Multiplier.java
@@ -1,9 +1,11 @@
 public class Multiplier {
-    int number;
-    public Multiplier (int number) {
+    private int number;
+
+    public Multiplier(int number) {
         this.number = number;
     }
-    public int multiply (int number) {
-        
+
+    public int multiply(int number) {
+        return number * this.number;
     }
 }
